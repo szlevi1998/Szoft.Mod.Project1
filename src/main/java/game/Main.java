80a6142e612e4fb2ae149f07c62e52ec05575651
udvardymarton <martonudvardy4@gmail.com>
2020-09-27 14:56:24 +0200

    program: word class(beta) kész.

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -5,7 +5,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Names name = new Names();
-        name.askConnectionQuery();
+        //name.askConnectionQuery();
         //name.askConnectionInsert();
+        Word first = new Word();
+        first.checkIt();
     }
 }
--- src/main/java/game/Word.java
+++ src/main/java/game/Word.java
@@ -1,4 +1,42 @@
 package game;
 
+import java.util.Scanner;
+
 public class Word {
+    Scanner scan = new Scanner(System.in);
+    String s = "kutya";
+    String c;
+    int tips = 0;
+    int counter = s.length();
+
+    public void checkIt() {
+        System.out.println(s);
+        while(counter > 0 && tips != 11){
+            c = scan.nextLine();
+            while (containSpecialCharacter(c) == true) {
+                System.out.println("You are wrong!, do it again.");
+                c = scan.nextLine();
+            }
+            if (s.contains(c)) {
+                counter -= 1;
+                s = s.replace(c,"");
+                System.out.println(s + " " + counter);
+            }
+            else {
+                tips++;
+                System.out.println(tips);
+            }
+        }
+        if (counter == 0){
+        System.out.println("You won!");
+        }
+        else {
+            System.out.println("You lost!");
+        }
+    }
+
+    public boolean containSpecialCharacter(String s) {
+        return (s == null) ? false : s.matches("[^a-z]");
+    }
+
 }
