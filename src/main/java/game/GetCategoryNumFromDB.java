58fa49f05677aa4095a198a2b7d403961a650b4e
Martin97124 <martin97124@gmail.com>
2020-10-04 16:28:26 +0200

    Program: Kategória kiíratás(2). KÉSZ

---
+++ src/main/java/game/GetCategoryNumFromDB.java
@@ -0,0 +1,19 @@
+package game;
+import DB.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class GetCategoryNumFromDB {
+    public static int categoryNumFromDB() throws SQLException {
+        Statement st;
+        ResultSet rs;
+        String query = "select fk_kategoria from szavak where szo = \'" + GetWordsFromDB.wordFromDB() + "\'";
+        st = Database.getCon().createStatement();
+        rs = st.executeQuery(query);
+        rs.next();
+        String categoryNum = rs.getString("fk_kategoria");
+        int categoryNumber = Integer.parseInt(categoryNum);
+        rs.close();
+        return categoryNumber;
+    }
+}
