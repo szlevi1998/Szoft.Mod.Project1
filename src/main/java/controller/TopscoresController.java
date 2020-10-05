a350913a20df7355de64e4d7962b72355692a1ed
szlevi1998 <sz.levi1998@gmail.com>
2020-10-05 09:31:19 +0200

    Kilépés gomb hozzáadása az eredménytáblánál.

--- src/main/java/controller/TopscoresController.java
+++ src/main/java/controller/TopscoresController.java
@@ -1,11 +1,13 @@
 package controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
@@ -23,11 +25,21 @@ public class TopscoresController {
     @FXML
     public TableColumn gamesWon;
 
+    @FXML
+    public Button exitButton;
+
+    @FXML
+    public Button backToGame;
+
     public void backToPlay(ActionEvent actionEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void closeProgram(ActionEvent actionEvent) {
+        exitButton.setOnAction(actionEvent1 -> Platform.exit());
     }
 }
